189273c59f167ef9af8bff8062f764c8c97a9372
Jonathas Germano <jonathas.germano@hotmail.com>
2024-03-20 23:58:49 -0300

    fix: assets imports at utils folder

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,31 +1,31 @@
-import hero from "./public/assets/images/hero.jpeg";
+import hero from "../../public/assets/images/hero.jpeg";
 
-import hmv from "./public/assets/videos/hero.mp4";
-import smallmv from "./public/assets/videos/smallHero.mp4";
-import highlightFirstmv from "./public/assets/videos/highlight-first.mp4";
-import highlightSectmv from "./public/assets/videos/hightlight-third.mp4";
-import highlightThirdmv from "./public/assets/videos/hightlight-sec.mp4";
-import highlightFourthmv from "./public/assets/videos/hightlight-fourth.mp4";
-import exploremv from "./public/assets/videos/explore.mp4";
-import framemv from "./public/assets/videos/frame.mp4";
+import hmv from "../../public/assets/videos/hero.mp4";
+import smallmv from "../../public/assets/videos/smallHero.mp4";
+import highlightFirstmv from "../../public/assets/videos/highlight-first.mp4";
+import highlightSectmv from "../../public/assets/videos/hightlight-third.mp4";
+import highlightThirdmv from "../../public/assets/videos/hightlight-sec.mp4";
+import highlightFourthmv from "../../public/assets/videos/hightlight-fourth.mp4";
+import exploremv from "../../public/assets/videos/explore.mp4";
+import framemv from "../../public/assets/videos/frame.mp4";
 
-import apple from "./public/assets/images/apple.svg";
-import search from "./public/assets/images/search.svg";
-import bag from "./public/assets/images/bag.svg";
-import watch from "./public/assets/images/watch.svg";
-import right from "./public/assets/images/right.svg";
-import replay from "./public/assets/images/replay.svg";
-import play from "./public/assets/images/play.svg";
-import pause from "./public/assets/images/pause.svg";
+import apple from "../../public/assets/images/apple.svg";
+import search from "../../public/assets/images/search.svg";
+import bag from "../../public/assets/images/bag.svg";
+import watch from "../../public/assets/images/watch.svg";
+import right from "../../public/assets/images/right.svg";
+import replay from "../../public/assets/images/replay.svg";
+import play from "../../public/assets/images/play.svg";
+import pause from "../../public/assets/images/pause.svg";
 
-import yellow from "./public/assets/images/yellow.jpg";
-import blue from "./public/assets/images/blue.jpg";
-import white from "./public/assets/images/white.jpg";
-import black from "./public/assets/images/black.jpg";
-import explore1 from "./public/assets/images/explore1.jpg";
-import explore2 from "./public/assets/images/explore2.jpg";
-import chip from "./public/assets/images/chip.jpeg";
-import frame from "./public/assets/images/frame.png";
+import yellow from "../../public/assets/images/yellow.jpg";
+import blue from "../../public/assets/images/blue.jpg";
+import white from "../../public/assets/images/white.jpg";
+import black from "../../public/assets/images/black.jpg";
+import explore1 from "../../public/assets/images/explore1.jpg";
+import explore2 from "../../public/assets/images/explore2.jpg";
+import chip from "../../public/assets/images/chip.jpeg";
+import frame from "../../public/assets/images/frame.png";
 
 export const heroImg = hero;
 
