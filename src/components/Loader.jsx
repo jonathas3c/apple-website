53892fb7df6dcd1cc701ab8f09aefd73dda4fba4
Jonathas Germano <jonathas.germano@hotmail.com>
2024-03-25 23:57:14 -0300

    feat: add loader, lighting layer and phone 3d model

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,13 @@
 import { Html } from '@react-three/drei'
-import React from 'react'
 
 const Loader = () => {
-  return (
-    <Html>
-      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">
-          Loading...
-        </div>
-      </div>
-    </Html>
-  )
+	return (
+		<Html>
+			<div className='absolute left-0 top-0 flex size-full items-center justify-center'>
+				<div className='size-[10vw] rounded-full'>Loading...</div>
+			</div>
+		</Html>
+	)
 }
 
 export default Loader
